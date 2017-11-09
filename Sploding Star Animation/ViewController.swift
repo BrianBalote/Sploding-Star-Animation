698693e6ed845529b111098537fefa31d9f7c287
Brian Rey Balote <brianreybalote@Brians-Mac-mini.local>
2017-11-09 12:33:16 +0800

    Initial Commit

---
+++ Sploding Star Animation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sploding Star Animation
+//
+//  Created by Brian Rey Balote on 09/11/2017.
+//  Copyright © 2017 Brian Rey Balote. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Sploding Star AnimationTests/Sploding_Star_AnimationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sploding_Star_AnimationTests.swift
+//  Sploding Star AnimationTests
+//
+//  Created by Brian Rey Balote on 09/11/2017.
+//  Copyright © 2017 Brian Rey Balote. All rights reserved.
+//
+
+import XCTest
+@testable import Sploding_Star_Animation
+
+class Sploding_Star_AnimationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
